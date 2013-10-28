560444166d839ac20a16a82e606344c6a977836f
kojitominaga <koji.tominaga@ibv.uio.no>
2013-10-28 11:25:54 +0100

    decided to handle errors in running scripts

--- R/utilities.R
+++ R/utilities.R
@@ -206,7 +206,8 @@ TemperatureAtDepths <- function(flakeresult, parameters, z) {
 HourlyAirTemperature <- function(d, lat, lon,
                                  startT, minT, maxT, meanT, minTn, maxTn,
                                  n = 24) {
-  !! TODO (see commit details on 2013-10-27)
+  ## decided to handle errors in running scripts
+  ## !! TODO (see commit details on 2013-10-27)
   require(oce)
   threshold <- ifelse(meanT < 273, 0.38, 0.30)
   if (!(n = 24)) {
